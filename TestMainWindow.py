41e1b988d885a67bfee4718a442d4e62196e3a6c
ngk0801 <khanhnvn234112e@st.uel.edu.vn>
2024-11-15 23:39:44 +0700

    Đã giải quyết xung đột

--- TestMainWindow.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-from MainWindowExt import MainWindowExt
-
-app=QApplication([])
-myWindow=MainWindowExt()
-myWindow.setupUi(QMainWindow())
-myWindow.show()
-app.exec()
