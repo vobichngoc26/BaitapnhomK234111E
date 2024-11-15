e87948de3c300bf95395c0780ed433a933f9edbb
unknown <=>
2024-11-16 00:16:53 +0700

    update the projets

--- MainWindowExt.py
+++ MainWindowExt.py
@@ -2,13 +2,11 @@ from MainWindow import Ui_MainWindow
 
 
 class MainWindowExt(Ui_MainWindow):
-    price_matle=300000
-    price_matnuoc=500000
     num_matle = 0
     num_matnuoc = 0
     money_matle = 0
     money_matnuoc = 0
-    total = 0
+
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
         self.MainWindow = MainWindow
@@ -21,16 +19,28 @@ class MainWindowExt(Ui_MainWindow):
         self.pushButtonMua.clicked.connect(self.process_mua)
 
     def process_mua(self):
-        n_hoa = int(self.lineEditSoluong.text())
-        if self.radioButtonDalat.isChecked():
-            money_dalat = n_hoa * 100000
-            self.num_dalat = self.num_dalat + n_hoa
-            self.money_dalat = self.money_dalat + money_dalat
-        elif self.radioButtonSapa.isChecked():
-            money_sapa = n_hoa * 100000
-            self.num_sapa = self.num_dalat + n_hoa
-            self.money_sapa = self.money_sapa + money_sapa
-        self.lineEditMatle.setText(str(self.num_dalat))
-        self.lineEditMatnuoc.setText(str(self.num_sapa))
-        self.lineEditThanhtien.setText(str(self.money_dalat))
-        self.lineEditTienHoaSapa.setText(str(self.money_sapa))
+        price_matle = 300000
+        price_matnuoc = 500000
+        try:
+            num_matle = int(self.lineEditMatle.text())
+        except ValueError:
+            num_matle = 0
+
+        try:
+            num_matnuoc = int(self.lineEditMatnuoc.text())
+        except ValueError:
+            num_matnuoc = 0
+
+        if self.radioButtonMatle.isChecked():
+            money_matle = num_matle* price_matle
+            self.num_matle = self.num_matle + num_matle
+            self.money_matle = self.money_matle + money_matle
+        elif self.radioButtonMatnuoc.isChecked():
+            money_matnuoc = num_matnuoc* price_matnuoc
+            self.num_matnuoc = self.num_matnuoc + num_matnuoc
+            self.money_matnuoc = self.money_matnuoc + money_matnuoc
+
+        self.lineEditMatle.setText(str(self.num_matle))
+        self.lineEditMatnuoc.setText(str(self.num_matnuoc))
+        self.lineEditThanhtienmatle.setText(str(self.money_matle))
+        self.lineEditThanhtienmatnuoc.setText(str(self.money_matnuoc))
--- TestMainWindow.py
+++ TestMainWindow.py
@@ -3,7 +3,8 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 from MainWindowExt import MainWindowExt
 
 app=QApplication([])
-myWindow=MainWindowExt()
-myWindow.setupUi(QMainWindow())
-myWindow.show()
+mainwindow=QMainWindow()
+myui=MainWindowExt()
+myui.setupUi(mainwindow)
+myui.showWindow()
 app.exec()
